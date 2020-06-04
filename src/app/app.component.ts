bf6c7cc19b422c96029e3f627b65a61161b15ff0
michal198711 <59646682+michal198711@users.noreply.github.com>
2020-06-04 11:14:34 +0200

    koniec
    koniec

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
-
+import {Hero} from './hero';
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <h1>{{title}}</h1>
+  <h2>My favorite hero is: {{myHero.name}}</h2>
+  <p>Heroes:</p>
+  <ul>
+    <li *ngFor="let hero of heroes">
+      {{ hero.name }}
+      </li>
+  </ul>
+  <p *ngIf="heroes.length > 3">There are many heroes!</p>
+`
 })
 export class AppComponent {
-  title = 'angular-tour-of-heroes';
+  title = 'Tour of Heroes';
+  heroes = [
+    new Hero(1, "Zuzia"),
+    new Hero(20, 'Ania'),
+    new Hero(13, "Mateusz"),
+    new Hero(15, "Łukasz"),
+
+  ];
+  myHero =  this.heroes[0];
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroComponent } from './hero/hero.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    HeroComponent,
+    HeroesComponent
+  ],
+  
   bootstrap: [AppComponent]
 })
 export class AppModule { }
